9fbd501240b8550bed968f046fd767c4a23412d5
moisesbaxin29-glitch <moisesbaxin29@gmail.com>
2026-01-16 09:37:47 -0500

    Added forms and added the local sortage and css

--- script.js
+++ script.js
@@ -5,26 +5,28 @@ var grid = document.querySelector(".grid-container");
 
 var xhttp = new XMLHttpRequest();
 
-xhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
+xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
 
         data = JSON.parse(xhttp.responseText);
         console.log(data);
+        localStorage.setItem("dataList", JSON.stringify(data));
 
-        data.forEach(function(movie){
+
+        data.forEach(function (movie) {
             let card = document.createElement("div");
             card.classList.add("card");
 
-            let textData = 
-            "<div class = 'movie-title'>" + movie.title + "</div>" + 
-            "<span>" + 
-            "Cast: "  + movie.cast + "<br>" +
-            "Release Date: " + movie.year + "<br>";
+            let textData =
+                "<div class = 'movie-title'>" + movie.title + "</div>" +
+                "<span>" +
+                "Cast: " + movie.cast + "<br>" +
+                "Release Date: " + movie.year + "<br>";
 
             card.innerHTML = textData;
 
-            if(movie.imgSrc){
-                card.style.backgroundImage = "url('" + movie.imgSrc +"')";
+            if (movie.imgSrc) {
+                card.style.backgroundImage = "url('" + movie.imgSrc + "')";
             }
 
             grid.appendChild(card);
